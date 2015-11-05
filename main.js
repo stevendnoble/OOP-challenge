fc5ffd0137c368c3c9700de6ea1c0156bd473a5c
stevennoble78 <stevennoble78@hotmail.com>
2015-11-05 00:07:28 -0800

    add website uberchallenge with handlebars

--- main.js
+++ main.js
@@ -12,19 +12,20 @@ $(document).ready(function() {
 	  this.smellsPretty = smellsPretty;
 	  this.parent1 = parent1 || this.name;
 	  this.parent2 = parent2 || this.name;
-	  this.imageURL = imageURL;
+	  this.imageURL = imageURL || "blankflower.jpg";
+	  this.description = '';
 	  // this.sniff = function() {
 	  //   console.log("Sniff Sniff Sniff!)");
 	  // };
 	  this.smellsGood = function(answer) {
 	    if (answer) {
-	      return "This flower smells amazing!";
+	      return "This flower smells amazing!  ";
 	    } else {
-	      return "What a noxious weed!";
+	      return "What a noxious weed!  ";
 	    }
 	  };
 	  this.describe = function() {
-	    var str = ("The "+this.name+" is a "+this.color +" flower with "+this.petals+".  ");
+	    var str = ("The "+this.name+" is a "+this.color +" flower with "+this.petals+" petals.  ");
 	    str += ("Typically, the "+this.name+" is about "+this.stemLength+" cm long with ");
 	    str += ("petals that are roughly "+petalSize+" cm.  ");
 	    str += this.smellsGood(smellsPretty);
@@ -69,11 +70,11 @@ $(document).ready(function() {
 
 	// Array of flowers
 	var flowers = [
-	  new Flower('delphinium', 'purple', 8, 2, 3, false, true),
-	  new Flower('tulip', 'violet', 6, 2, 6, false, true),
-	  new Flower('rose', 'red', 22, 3, 12, false, true),
-	  new Flower('primrose', 'yellow', 4, 3, 5, false, false),
-	  new Flower('venus flytrap', 'green-red', 2, 3, 4, true, false)
+	  new Flower('delphinium', 'purple', 8, 2, 3, false, true, false, false, 'delphinium.jpg'),
+	  new Flower('tulip', 'violet', 6, 2, 6, false, true, false, false, 'tulip.jpg'),
+	  new Flower('rose', 'red', 22, 3, 12, false, true, false, false, 'rose.jpg'),
+	  new Flower('primrose', 'yellow', 4, 3, 5, false, false, false, false, 'primrose.jpg'),
+	  new Flower('venus flytrap', 'green-red', 2, 3, 4, true, false, false, false, 'venusflytrap.jpg')
 	];
 
 	var size = flowers.length;
@@ -90,11 +91,29 @@ $(document).ready(function() {
 	  }
 	}
 
+	for(i=0; i<flowers.length; i++) {
+		flowers[i].description = flowers[i].describe();
+	}
+
+	Handlebars.registerHelper('grouped_each', function(every, context, options) {
+    var out = "", subcontext = [], i;
+    if (context && context.length > 0) {
+        for (i = 0; i < context.length; i++) {
+            if (i > 0 && i % every === 0) {
+                out += options.fn(subcontext);
+                subcontext = [];
+            }
+            subcontext.push(context[i]);
+        }
+        out += options.fn(subcontext);
+    }
+    return out;
+	});
+
 	// compile handlebars template
 	var source = $('#flowers-template').html();
 	var template = Handlebars.compile(source);
-	// var flowersHTML = template(flowers.slice(i, i+4));
-	$('#results').append(template(flowers.slice(i, i+4)));
+	$('#results').append(template({flowers: flowers}));
 });
 
 
